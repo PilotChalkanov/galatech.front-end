0ded4dbbac4683ab2e41b9f02c2e02829ba9dcd3
PilotChalkanov <nik.chalkanov@gmail.com>
2023-03-25 17:49:48 +0200

    Google maps on the home page

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -16,7 +16,7 @@ import {
 export const userLoginReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_LOGIN_REQUEST:
-            return {loading: true, products: []}
+            return {loading: true}
 
         case USER_LOGIN_SUCCESS:
             return {loading: false, userInfo: action.payload}
@@ -35,7 +35,7 @@ export const userLoginReducer = (state = {}, action) => {
 export const userSignupReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_SIGNUP_REQUEST:
-            return {loading: true, products: []}
+            return {loading: true}
 
         case USER_SIGNUP_SUCCESS:
             return {loading: false, userInfo: action.payload}
@@ -48,6 +48,22 @@ export const userSignupReducer = (state = {}, action) => {
 
         case USER_LOGOUT:
             return {}
+
+        default:
+            return state
+    }
+}
+
+export const userDetailsReducer = (state = {user:{}}, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return {...state, loading: true}
+
+        case USER_DETAILS_SUCCESS:
+            return {loading: false, user: action.payload}
+
+        case USER_DETAILS_FAIL:
+            return {loading: false, error: action.payload}
 
         default:
             return state
